16db7423718a82c141e385ae6cc564f0b7d74f58
Cédric Donner <cedonner@gmail.com>
2013-10-13 22:26:37 +0200

    start adding level 2

---
+++ defis/binaire2decimal.py
@@ -0,0 +1,16 @@
+'''
+
+Dans ce fichier Python, vous devez écrire un programme qui lit sur l'entrée
+standard une chaine de caractères représentation une chaine binaire (exemple
+10010010101) et indiquer la valeur décimale du nombre représenté
+
+'''
+
+binaire = input()
+
+decimal = 0
+
+# compléter votre code ici
+
+
+print(decimal)
---
+++ defis/nombre_secret.py
@@ -0,0 +1,10 @@
+'''
+
+Défi 2 : écrire un programme Python permettant de trouver avec le moins
+d'essais possibles le nombre secret. Pour ceci, il suffit d'appeler l'url
+donnerc.kd.io/secret/14 si vous pensez que le nombre secret est 14. 
+
+Le serveur HTTP qui traite la requête envoie
+
+* ``plus`` si le nombre est plus grand
+* moins 
